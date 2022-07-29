afb7bcf031497bc6480d445aaa55b3113e10e11d
Josue Canoeira <titocanoeira@gmail.com>
2022-07-29 21:48:40 +0100

    add new changes

--- 06-Modal-project/starter/script.js
+++ 06-Modal-project/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const btnOpenModal = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+
+
+
+for (let index = 0; index < btnOpenModal.length; index += 1)
+    btnOpenModal[index].addEventListener('click', function () {
+        modal.classList.remove('hidden');
+        overlay.classList.remove('hidden');
+    });
+
